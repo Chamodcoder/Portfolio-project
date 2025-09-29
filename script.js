afba686bee4f141e8dd2726ef5e497de70994621
R.G. Chamod chathuranga <chamodchathuranga2002@gmail.com>
2025-09-29 23:45:58 +0530

    modified

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
-let navLinks = document. querySelector('header nav a');
+let navLinks = document.querySelectorAll('header nav a');
 
-window.console = () => {
+window.addEventListener('scroll', () => {
+    let top = window.scrollY;
     sections.forEach(sec => {
-        let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id  = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active')
-            })
+        let id = sec.getAttribute('id');
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href').includes(id)) {
+                    link.classList.add('active');
+                }
+            });
         }
-    })
-}
+    });
+});
 
-menuIcon.oneclick = () => {
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
